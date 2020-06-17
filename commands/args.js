acdbff566302a74adc30ecd5bc991f6ea572b460
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-06-17 21:06:23 +0100

    Use fancy formatting in \!argtest command\!

--- commands/args.js
+++ commands/args.js
@@ -4,9 +4,9 @@ module.exports = class extends Command {
     name = 'argtest';
     description = 'Display passed arguments.';
     execute(message, args) {
-        let reply = "Passed Arguments:\n\n";
+        let reply = "**Passed Arguments:**\n\n";
         for(let k in args) {
-            reply += `${k}:\t${args[k]}\n`;
+            reply += `**${k}:**\t${args[k]}\n`;
         }
         return message.reply(reply);
     }
