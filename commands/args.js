57a52fb03648d9d3f621750a10f29438332efb76
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-06-17 20:24:01 +0100

    Add !argtest command (args.js) to test argument parsing.

---
+++ commands/args.js
@@ -0,0 +1,13 @@
+const Command = require('../command');
+
+module.exports = class extends Command {
+    name = 'argtest';
+    description = 'Display passed arguments.';
+    execute(message, args) {
+        let reply = "Passed Arguments:\n\n";
+        for(let k in args) {
+            reply += `${k}:\t${args[k]}\n`;
+        }
+        return message.reply(reply);
+    }
+};
