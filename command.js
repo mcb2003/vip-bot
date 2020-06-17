f3ed62a5f5f41b5cc7b5df1124f496a9eda6620c
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-06-17 21:28:35 +0100

    Allow commands to provide help documentation.
    This documentation is automatically returned when passing -h or --help to any command.

--- command.js
+++ command.js
@@ -2,14 +2,32 @@ const yargsParser = require('yargs-parser');
 const deepmerge = require('deepmerge');
 
 module.exports = class {
+    usage = "";
+
     constructor(config, defaultArgConfig) {
+        this.globalConfig = config;
         this.argConfig = deepmerge(defaultArgConfig, (this.argConfig || {}));
     }
 
     run(message, rawArgs) {
         // Parse the raw command arguments
         const args = yargsParser(rawArgs, this.argConfig);
+        // If the user wants help, display it
+        if(args.help) {
+            return this.sendHelp(message);
+        }
         // Future checking (E.G. guild only, user / channel perms, cool-down, etc)
         this.execute(message, args);
+    }
+    
+    sendHelp(message) {
+        let reply = `**${this.name}:** ${this.description}`;
+            reply += `\n**Usage:** ${this.globalConfig.prefix}${this.name.toLowerCase()} ${this.usage}`;
+        if(this.help) {
+            reply += '\n' + this.help;
+        } else {
+            reply += "\nThis command doesn't provide any further help documentation.";
+        }
+        return message.channel.send(reply);
     }
 };
--- commands/args.js
+++ commands/args.js
@@ -3,6 +3,8 @@ const Command = require('../command');
 module.exports = class extends Command {
     name = 'argtest';
     description = 'Display passed arguments.';
+    usage = '<your arguments here>';
+    help = "Provide this command with any arguments you wish, and it will return the parsed representation of your arguments.";
     execute(message, args) {
         let reply = "**Passed Arguments:**\n\n";
         for(let k in args) {
